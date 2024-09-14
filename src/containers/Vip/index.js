40bb71725e419f9e60a6f237eb8e3a799ef6668e
ZHHHuaZhang <545219616@qq.com>
2024-09-14 19:48:22 +0800

    fix bug

--- src/containers/Vip/index.js
+++ src/containers/Vip/index.js
@@ -1,20 +1,9 @@
-import React,{ useState,useEffect } from "react";
-import { useParams} from "react-router-dom";
-import { Card } from 'antd';
-import axios from "axios";
+import React from "react";
 import './style.css'
 function Vip(){
     return(
         <div className='vip'>vip</div>
     )
 }
-// class Detail extends Component {
-//     render() {
-//         return <div>Detail</div>
-//     }
-//     componentDidMount() {
-//         console.log(this.props)
-//     }
-// }
 
 export default Vip
